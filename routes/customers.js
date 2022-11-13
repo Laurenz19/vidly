17ffc1cb70be9900122c117ab72d2ca5b23cc1e3
laurenz19 <laurenziosambany@gmail.com>
2022-11-13 16:25:49 +0300

    customer api available

--- routes/customers.js
+++ routes/customers.js
@@ -35,20 +35,61 @@ router.get('/', async(req, res)=>{
     res.send(customers)
 })
 
-router.get('/:id', (req, res)=>{
-    
+router.get('/:id', async(req, res)=>{
+    const customer = await Customer.findById(req.params.id)
+    if(!customer) return res.status(404).send('Customer with given ID was not found')
+
+    res.send(customer)
 })
 
-router.post('/:id', (req, res)=>{
-    
+router.post('/', async(req, res)=>{
+    const  {error} = validateCustomer(req.body)
+    if(error) return res.status(400).send(error.details[0].message)
+
+    const customer = new Customer({
+        name: req.body.name,
+        isGold: req.body.isGold,
+        phone: req.body.phone
+    })
+
+    try {
+        const result = await customer.save()
+        res.send(result) 
+    } catch (err) {
+        res.status(400).send(err.errors.message)
+    }
 })
 
-router.put('/:id', (req, res)=>{
-    
+router.put('/:id', async(req, res)=>{
+    const  {error} = validateCustomer(req.body)
+    if(error) return res.status(400).send(error.details[0].message)
+
+    const customer = await Customer.findByIdAndUpdate(req.params.id, {
+        name: req.body.name,
+        isGold: req.body.isGold,
+        phone: req.body.phone
+    })
+
+    if(!customer) return res.status(404).send('Customer with given ID was not found')
+
+    res.send(await Customer.findById(req.params.id))
 })
 
-router.delete('/:id', (req, res)=>{
-    
+router.delete('/:id', async(req, res)=>{
+    const customer = await Customer.findByIdAndRemove(req.params.id)
+
+    if(!customer) return res.status(404).send('Customer with given ID was not found')
+
+    res.send(customer)
 })
 
+function validateCustomer(customer){
+    const schema = Joi.object({
+        name: Joi.string().min(3).required(),
+        isGold: Joi.boolean(),
+        phone: Joi.string().required()
+    })
+
+    return schema.validate(customer)
+}
 module.exports = router
